ec7cbb92cddac215b30f2f63c50e156fb59eb912
Lisbeth Ortega <118750598+lisbeth-op@users.noreply.github.com>
2023-01-24 00:06:00 -0400

    Add files via upload

---
+++ Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Factorial
+{
+   
+
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int resultado = factorial(num);
+        Console.WriteLine($"\n\n\nEl factorial de [{num}]es {resultado}.\n\n\n ");
+       
+    }
+
+
+
+
+    static int factorial(int numero)
+    {
+        return Enumerable.Range(1, numero).Aggregate(1, (acc, x) => acc * x);
+    }
+
+
+}
+
